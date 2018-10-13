c67161d7ec00901b9071429e05baa598b730f6df
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-10-13 15:40:53 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_instance)
+    self.backers << backer_instance
+    backer_instance.backed_projects << self
+  end
+end
